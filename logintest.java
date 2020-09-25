3fef6ed4fbcc94a7ebc4dd50b25e52d275334709
singharpit47 <71885970+singharpit47@users.noreply.github.com>
2020-09-25 19:00:00 +0530

    Create logintest.java

---
+++ logintest.java
@@ -0,0 +1,23 @@
+package Testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	
+	public static void main(String args[]) throws Exception{
+		
+		System.setProperty("webdriver.chrome.driver","$JENKINS_HOME/tools/chromedriver/chromedriver.ext");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("email")).sendKeys("xyz");
+		driver.findElement(By.id("pass")).sendKeys("xyz");
+		driver.findElement(By.id("u_0_b")).click();
+		
+	}
+
+}
